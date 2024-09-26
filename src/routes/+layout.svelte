acf1d6a645edce3eb4d7a66d7f1fca5a4675b96b
Ben Mitchinson <mitchinson.dev@gmail.com>
2024-09-25 22:37:45 -0500

    home screen layout adjust w footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,23 @@
 	import '../app.css';
 </script>
 
-<header class="bg-white bg-opacity-70 backdrop-blur-md shadow-lg p-4">
-	<a href="/"><h1 class="text-3xl font-bold text-center">Quiz App</h1></a>
-</header>
-<main>
-	<div class="min-h-screen overflow-hidden">
-		<slot />
-	</div>
-</main>
+<div
+	class="flex flex-col items-center justify-between h-screen bg-gradient-to-r from-blue-200 to-blue-400 backdrop-blur-md"
+>
+	<header class="bg-white bg-opacity-70 backdrop-blur-md shadow-lg p-4 w-full">
+		<a href="/"><h1 class="text-3xl font-bold text-center">Quiz App</h1></a>
+	</header>
+
+	<main>
+		<div class="overflow-hidden flex items-center justify-center">
+			<slot />
+		</div>
+	</main>
+
+	<footer class="w-full text-center bg-white text-gray-700 p-3">
+		<p>
+			For feature requests, access requests, or bug reports, please contact mitchinson.dev@gmail.com
+		</p>
+		<p>Made w 🩵 in Des Moines 🌽</p>
+	</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,14 @@
-<div
-	class="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-200 to-blue-400 backdrop-blur-md"
->
-	<div class="bg-white bg-opacity-70 rounded-lg p-8 shadow-lg">
-		<div class="flex flex-col space-y-2">
-			<a
-				href="/admin/students"
-				class="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 transition"
-				>Admin: Manage Students</a
-			>
-			<a
-				href="/student"
-				class="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-600 transition"
-				>Take a Quiz</a
-			>
-		</div>
+<div class="bg-white bg-opacity-70 rounded-lg p-8 shadow-lg">
+	<div class="flex flex-col space-y-2">
+		<a
+			href="/admin/students"
+			class="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 transition"
+			>Admin: Manage Students</a
+		>
+		<a
+			href="/student"
+			class="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-600 transition"
+			>Take a Quiz</a
+		>
 	</div>
 </div>
