dab4ef4171049b7a4e878fdc12bd426cc0295804
Ben Mitchinson <mitchinson.dev@gmail.com>
2024-09-27 21:30:03 -0500

    Add some playwright tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,46 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
+const teacherQuizPrompt = 'Teacher: Manage Quizzes';
+const teacherStudentPrompt = 'Teacher: Manage Students';
+const studentPrompt = 'Student: Take a Quiz';
+
+test.beforeEach(async ({ context }) => {
+	await context.clearCookies();
+});
+
+test('Home page has the right links', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+	await expect(page.locator(`a:has-text("${teacherQuizPrompt}")`)).toBeVisible();
+	await expect(page.locator(`a:has-text("${teacherStudentPrompt}")`)).toBeVisible();
+	await expect(page.locator(`a:has-text("${studentPrompt}")`)).toBeVisible();
+});
+
+test('Manage students page is protected by password', async ({ page }) => {
+	await page.goto('/admin/students');
+
+	await expect(page.locator(`h1:has-text("Teacher Login")`)).toBeVisible();
+});
+
+test('Manage quizzes page is protected by password', async ({ page }) => {
+	await page.goto('/admin/quizzes');
+
+	await expect(page.locator(`h1:has-text("Teacher Login")`)).toBeVisible();
+});
+
+test('Invalid admin password shows message', async ({ page }) => {
+	await page.goto('/admin/quizzes');
+
+	await page.locator(`input`).fill('wrong-password');
+	await page.locator(`button:has-text("Submit")`).click();
+
+	await expect(page.getByText('Incorrect password')).toBeVisible();
+});
+
+test('Correct admin password goes to page', async ({ page }) => {
+	await page.goto('/admin/quizzes');
+
+	await page.locator(`input`).fill('admin');
+	await page.locator(`button:has-text("Submit")`).click();
+
+	await expect(page.locator(`h1:has-text("Quiz Management")`)).toBeVisible();
 });
