fbd8cce01887bc20a131a988895da1b204ebf1bb
Ben Mitchinson <mitchinson.dev@gmail.com>
2024-11-10 21:01:28 -0600

    Adjust add students screen

--- src/routes/teacher/manageStudents/+page.svelte
+++ src/routes/teacher/manageStudents/+page.svelte
@@ -58,21 +58,19 @@
 <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
 	<h1 class="text-2xl font-bold mb-6 text-center">Manage Students</h1>
 	<form method="post" action="?/addStudents" class="space-y-4">
-		<div>
-			<label for="lastNames" class="block text-gray-700 font-medium mb-2">
-				Enter Student First Initial + Last Name (one per line):
-			</label>
-			<p>This name is what a student will enter as their "name" to take their quiz</p>
-			<textarea
-				id="lastNames"
-				name="lastNames"
-				rows="10"
-				class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#26561b]"
-				placeholder="jmitchell
+		<label for="lastNames" class="block text-gray-700 font-medium mb-2">
+			Enter Student First Initial + Last Name (one per line):
+		</label>
+		<p>This name is what a student will enter as their "name" to take their quiz</p>
+		<textarea
+			id="lastNames"
+			name="lastNames"
+			rows="10"
+			class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#26561b]"
+			placeholder="jmitchell
 jdenver
 bdylan"
-			></textarea>
-		</div>
+		></textarea>
 		<div class="flex justify-center">
 			<button
 				type="submit"
