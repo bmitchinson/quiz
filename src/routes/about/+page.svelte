ea0df295301b8404702be4ba68093196658d06ee
Ben Mitchinson <mitchinson.dev@gmail.com>
2024-11-11 22:49:02 -0600

    About page cleanup

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,39 +1,45 @@
 <script>
+	import Break from '$lib/components/Break.svelte';
 	import Wave from '$lib/components/Wave.svelte';
 </script>
 
 <div class="bg-white rounded-lg p-8 shadow-lg max-w-lg">
 	<div class="flex flex-col space-y-4">
 		<Wave />
-		<hr class="self-center w-16 border-t border-gray-300" />
+		<Break />
 		<p class="text-base">
 			I wrote this app for students to take math quizzes in an effort to save teachers time grading.
 		</p>
-		<hr class="self-center w-16 border-t border-gray-300" />
+
+		<Break />
 
 		<p class="text-base">
 			If it's broken in any way, let me know! If this site is making your job difficult, I'd like to
 			address that ASAP.
 		</p>
 
-		<hr class="self-center w-16 border-t border-gray-300" />
+		<Break />
 
 		<p class="text-base">
 			I'm also available for further consulting work, keep me in mind if you'd like fast web
 			applications built in 2 week timelines.
 		</p>
 
-		<hr class="self-center w-16 border-t border-gray-300" />
+		<Break />
 
-		<p class="text-base">
-			- Ben Mitchinson <a href="https://mitchinson.dev" style="color: cornflowerblue">
-				(https://mitchinson.dev)
-			</a> mitchinson.dev@gmail.com
-		</p>
-		<p>
-			<a href="https://github.com/bmitchinson/quiz" style="color: cornflowerblue">
-				Link to Source Code
+		<div class="text-center">
+			Made w/ 💚 in Des Moines 🌽 - <a
+				href="https://github.com/bmitchinson/quiz"
+				style="color: cornflowerblue"
+			>
+				Source Code
 			</a>
-		</p>
+			<div class="mt-4">
+				Ben Mitchinson
+				<a href="https://mitchinson.dev" style="color: cornflowerblue">
+					(https://mitchinson.dev)
+				</a> mitchinson.dev@gmail.com
+			</div>
+		</div>
 	</div>
 </div>
