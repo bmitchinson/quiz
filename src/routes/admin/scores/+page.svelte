377237e93882cb87be58a23eb9e59bf403929352
Ben Mitchinson <mitchinson.dev@gmail.com>
2024-11-28 11:42:14 -0600

    UI submits selected teacher

--- src/routes/admin/scores/+page.svelte
+++ src/routes/admin/scores/+page.svelte
@@ -6,19 +6,24 @@
 	import { grades } from '$lib/components/RadioButtons';
 	import { onMount } from 'svelte';
 
+	export let data;
+	$: teacherOptions = data.teachers
+		.filter((teacher) => teacher.grade === parseInt(selectedGrade))
+		.map((teacher) => teacher.name);
+
 	let sampleData: QuizScoreSummaryDataPoint[] = [];
 
-	let onGradeChange = (n: string) => {};
+	let fetchQuizScoreSummary = (grade: string, teacher: string) => {};
 
 	onMount(() => {
-		onGradeChange = (newSelectedGrade: string) => {
+		fetchQuizScoreSummary = (grade: string, teacher: string) => {
 			tableLoading = true;
 			fetch('/api/quiz/getSummary', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify({ grade: newSelectedGrade })
+				body: JSON.stringify({ grade, teacher })
 			})
 				.then((res) => res.json())
 				.then((data) => {
@@ -41,16 +46,28 @@
 	});
 
 	let selectedGrade = '1';
-	$: onGradeChange(selectedGrade);
+	let selectedTeacher = 'all';
+	$: fetchQuizScoreSummary(selectedGrade, selectedTeacher);
 
 	let tableLoading = false;
 	let dataExists = true;
 </script>
 
 <Card id={'scorechart-card'} additionalClasses={'w-5/6'}>
-	<div class="flex flex-row items-center justify-center space-x-4">
-		<p class="block text-gray-700 font-medium">Select Grade:</p>
-		<RadioButtons name="grade" options={grades} bind:selectedOptionValue={selectedGrade} />
+	<div class="flex flex-row items-center justify-center space-x-4 flex-wrap">
+		<div class="flex flex-row items-center space-x-4">
+			<p class="block text-gray-700 font-medium">Grade:</p>
+			<RadioButtons name="grade" options={grades} bind:selectedOptionValue={selectedGrade} />
+		</div>
+		<div class="flex flex-row items-center w-96 space-x-4">
+			<span class="block font-semibold">Teacher:</span>
+			<select bind:value={selectedTeacher} class="w-full px-3 py-2 border rounded-md" required>
+				<option value="all" selected>All Teachers</option>
+				{#each teacherOptions as teacher}
+					<option value={teacher}>{teacher}</option>
+				{/each}
+			</select>
+		</div>
 	</div>
 	<ScoreChart
 		canvasId="chartContainer"
