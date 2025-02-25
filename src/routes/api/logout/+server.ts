bdf488697e6feb28ad71d8ebe59efdc59b947898
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-02-24 22:13:30 -0600

    Log logout events

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,7 +1,11 @@
 import { clearCookies } from '$lib/passwordUtils';
 import { json } from '@sveltejs/kit';
+import { logEvent } from '$lib/logging.js';
+import { getSignedCookieValue } from '$lib/signedCookie.js';
 
 export const POST = async ({ cookies }) => {
+	const loginName = await getSignedCookieValue('loginName', cookies);
+	logEvent(loginName, 'Logged Out');
 	clearCookies(cookies);
 	return json({});
 };
