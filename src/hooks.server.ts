c617159d3b932587bc73852c81acc3e6f812c319
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-03-01 17:39:12 -0600

    note to self

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,10 @@ export function handleError({ error, event }) {
 	// for this, who cares.
 	logSvelteError(error, event);
 
+	// todo: I should probably not log errors in the database level, and just tag
+	// them so that they're logged more appropriately here. Current setup will
+	// log twice.
+
 	// Let SvelteKit handle the error normally
 	return;
 }
