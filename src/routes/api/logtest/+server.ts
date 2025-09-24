1dc1c9c1bccd5f2aea8ab23641ee65ed9f3c23fe
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-09-24 15:19:05 -0700

    add /api/logtest

---
+++ src/routes/api/logtest/+server.ts
@@ -0,0 +1,32 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+	const lokiUrl = process.env.LOKI_URL! + '/loki/api/v1/push'; // e.g. https://logs.mitchinson.dev/loki/api/v1/push
+	const basicAuth = process.env.LOKI_BASICAUTH!; // "admin:password"
+
+	const authHeader = 'Basic ' + Buffer.from(basicAuth).toString('base64');
+
+	const ts = Date.now() * 1e6; // ns timestamp
+	const payload = {
+		streams: [
+			{
+				stream: { app: 'vercel-app', level: 'info' },
+				values: [[ts.toString(), 'hello from vercel/sveltekit']]
+			}
+		]
+	};
+
+	const res = await fetch(lokiUrl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: authHeader
+		},
+		body: JSON.stringify(payload)
+	});
+
+	return json({
+		lokiStatus: res.status,
+		lokiStatusText: res.statusText
+	});
+}
